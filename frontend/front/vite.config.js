c94dbc13cacab4389ad976aedc0aa7a45f76bb06
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2025-11-05 21:32:08 +0530

    proto

--- frontend/front/vite.config.js
+++ frontend/front/vite.config.js
@@ -5,19 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        format: 'es'
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'text/javascript'
-    }
-  }
 })
