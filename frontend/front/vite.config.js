b585386a26d56fbdcf1ef7bb1a2a032ad6deb244
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2025-11-05 20:37:13 +0530

    proto

--- frontend/front/vite.config.js
+++ frontend/front/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+  },
+  build: {
+    modulePreload: true,
+    target: 'esnext',
+    minify: 'esbuild'
+  }
 })
